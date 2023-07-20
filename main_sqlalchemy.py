090f8c6b656cd6a2e147f449b63c6c4e50dc3ccf
youenn_wilson <117467104+youennwilson91@users.noreply.github.com>
2023-07-20 17:23:45 +0200

    Update main_sqlalchemy.py

--- main_sqlalchemy.py
+++ main_sqlalchemy.py
@@ -1,15 +1,17 @@
+
 import pandas as pd
 import spotipy
 from sqlalchemy import create_engine
 from spotipy.oauth2 import SpotifyOAuth
-from datetime import datetime
 from sqlalchemy.orm import Session
-from sql_tables import TopTracks, TopArtists, RecentTracks, RelatedArtists
+from sql_tables import TopTracks, TopArtists, RecentTracks, RelatedArtists, ArtistsImageUrls
 import os
 import uuid
 import time
 import psycopg2
 import fastparquet
+from datetime import datetime, timezone
+import pytz
 
 scope = "user-top-read user-library-read user-read-recently-played"
 sp = spotipy.Spotify(
@@ -37,7 +39,6 @@ for directory in directories:
         os.makedirs(directory)
 
 # Define functions
-
 def write_df_to_postgres(df, table_name):
     df.to_sql(table_name, engine, if_exists='append', index=False)
 
@@ -50,55 +51,15 @@ def write_data(df, table_name, base_path):
     write_df_to_postgres(df, table_name)
     write_df_to_parquet(df, base_path)
 
-def process_related_artists(sp, artist_id, artist_name, existing_pairs):
-    """
-    Retrieves related artists for a given artist, checks if the artist-related_artist pair
-    already exists in the set of existing pairs, and adds the related artist data to a list.
-
-    Args:
-        sp (Spotipy.Spotify): An instance of the Spotipy Spotify client.
-        artist_id (str): The ID of the artist.
-        artist_name (str): The name of the artist.
-        existing_pairs (set): A set of existing artist-related artist pairs.
-
-    Returns:
-        tuple: A tuple containing a list of dictionaries representing the related artists' data
-               and an updated set of existing artist-related artist pairs.
-    """
-    related_artists_data = []
-    related_artists = sp.artist_related_artists(artist_id)
-    for related in related_artists['artists']:
-        pair = (artist_name, related['name'])
-        if pair not in existing_pairs:
-            related_artists_data.append({
-                'artist_name': artist_name,
-                'related_artist': related['name'],
-                'id_version': f"{uuid.uuid4()}"
-            })
-            existing_pairs.add(pair)
-    return related_artists_data, existing_pairs
+def convert_to_paris_time(utc_timestamp):
+    utc_time = datetime.fromisoformat(utc_timestamp.replace('Z', '+00:00'))  # replace 'Z' with '+00:00' to form a valid isoformat string
+    utc_time = utc_time.replace(tzinfo=timezone.utc)  # specify current timezone as UTC
+    paris_time = utc_time.astimezone(pytz.timezone('Europe/Paris'))  # convert to Paris time
+    return paris_time.strftime('%Y-%m-%d %H:%M:%S')  # convert to string in the desired format
 
 def get_top_artists(term):
-    """
-    Fetches the 20 top artists for a user for each term (short_term, medium_term, or long_term),
-    adds them to the top_artists table, and updates the related_artists table.
-
-    Parameters:
-    term (str): The term for which to fetch top artists. One of 'short_term', 'medium_term', or 'long_term'.
-
-    Returns:
-    List[dict]: A list of dictionaries, each representing a top artist.
-    """
-
     artists = []
-    related_artists_data = []
-
-    # Create a new session
-    session = Session(bind=engine)
-
-    # Get the existing unique tuples of artist_name and related_artist
-    existing_pairs = {(tup.artist_name, tup.related_artist)
-                      for tup in session.query(RelatedArtists.artist_name, RelatedArtists.related_artist)}
+    artist_ids = []
 
     results = sp.current_user_top_artists(20, time_range=term)
     for idx, item in enumerate(results['items']):
@@ -106,50 +67,25 @@ def get_top_artists(term):
         artist['rank'] = f"{idx}"
         artist['id'] = item["id"]
         artist['name'] = item['name']
-        artist['genre'] = item["genres"]
+        artist['genre'] = ', '.join(item["genres"])  # Convert list to string
         artist['popularity'] = item["popularity"]
         artist_top_tracks = sp.artist_top_tracks(artist['id'])
-        artist['top_tracks'] = [tracks['name'] for tracks in artist_top_tracks['tracks']]
+        artist['top_tracks'] = ', '.join([tracks['name'] for tracks in artist_top_tracks['tracks']])  # Convert list to string
         artist['term'] = term
         artist["time"] = datetime.now()
-        # Generate 'id-version' column
         artist['id_version'] = f"{uuid.uuid4()}"
 
-        related_artists_data, existing_pairs = process_related_artists(sp, artist['id'], artist['name'], existing_pairs)
-
         artists.append(artist)
+        artist_ids.append(artist['id'])
 
     df = pd.DataFrame(artists)
-    related_artists_df = pd.DataFrame(related_artists_data)
-
     write_data(df, 'top_artists', "C:/SparkCourse/spotify/checkpoints/topartists")
-    write_data(related_artists_df, 'related_artists', "C:/SparkCourse/spotify/checkpoints/related_artists")
+
+    return artist_ids
 
 def get_top_tracks(term):
-    """
-    Fetches the 20 top tracks for a user for each term (short_term, medium_term, or long_term),
-    adds them to the top_tracks table, and updates the related_artists table.
-
-    Parameters:
-    term (str): The term for which to fetch top tracks. One of 'short_term', 'medium_term', or 'long_term'.
-
-    Returns:
-    List[dict]: A list of dictionaries, each representing a top track.
-    """
-
     tracks = []
-    related_artists_data = []
-
-    # Create a new session
-    session = Session(bind=engine)
-
-    # Get the existing unique tuples of rank, id and term
-    existing_tuples = {(tup.rank, tup.id, tup.term)
-                      for tup in session.query(TopTracks.rank, TopTracks.id, TopTracks.term)}
-
-    # Get the existing unique tuples of artist_name and related_artist
-    existing_pairs = {(tup.artist_name, tup.related_artist)
-                      for tup in session.query(RelatedArtists.artist_name, RelatedArtists.related_artist)}
+    artist_ids = []
 
     results = sp.current_user_top_tracks(20, time_range=term)
     for idx, item in enumerate(results['items']):
@@ -168,58 +104,21 @@ def get_top_tracks(term):
         track['explicit'] = item["explicit"]
         track['term'] = term
         track["time"] = datetime.now()
-
-        # Generate 'id-version' column
         track['id_version'] = f"{uuid.uuid4()}"
 
-        tup = (track['rank'], track['id'], track['term'])
-
-        if tup not in existing_tuples:
-            tracks.append(track)
-
-        # Get related artists for the artist of the current track
-        related_artists_data, existing_pairs = process_related_artists(sp, item["artists"][0]["id"], track['artist'],
-                                                                       existing_pairs)
+        tracks.append(track)
+        artist_ids.append(item["artists"][0]["id"])
 
     df = pd.DataFrame(tracks)
-    related_artists_df = pd.DataFrame(related_artists_data)
-
     write_data(df, 'top_tracks', "C:/SparkCourse/spotify/checkpoints/toptracks")
-    write_data(related_artists_df, 'related_artists', "C:/SparkCourse/spotify/checkpoints/related_artists")
 
-
-    # Close the session
-    session.close()
-
-    return tracks
-
+    return artist_ids
 
 def get_recent_tracks():
-    """
-    Fetches the 20 recently played tracks for a user, adds them to the recent_tracks table,
-    and updates the related_artists table.
-
-    Returns:
-    List[dict]: A list of dictionaries, each representing a recently played track.
-    """
-
     tracks = []
-    related_artists_data = []
+    artist_ids = []
 
-    # Create a new session
-    session = Session(bind=engine)
-
-    # Fetch the recent tracks from Spotify
     results = sp.current_user_recently_played()
-
-    # Get the existing unique pairs of id and played_at
-    existing_pairs = {(pair.id, pair.played_at)
-                      for pair in session.query(RecentTracks.id, RecentTracks.played_at)}
-
-    # Get the existing unique tuples of artist_name and related_artist
-    existing_related_pairs = {(tup.artist_name, tup.related_artist)
-                      for tup in session.query(RelatedArtists.artist_name, RelatedArtists.related_artist)}
-
     for idx, item in enumerate(results['items']):
         track = {}
         track['name'] = item["track"]["name"]
@@ -233,31 +132,94 @@ def get_recent_tracks():
         track['key'] = analysis["track"]["key"]
         track['loudness'] = analysis["track"]["loudness"]
         track['explicit'] = item["track"]["explicit"]
-        track['played_at'] = item['played_at']
+        track['played_at'] = convert_to_paris_time(item['played_at'])
         track["time"] = datetime.now()
-
-        # Generate 'id-version' column
         track['id_version'] = f"{uuid.uuid4()}"
 
-        pair = (track['id'], track['played_at'])
-
-        if pair not in existing_pairs:
-            tracks.append(track)
-
-        # Get related artists for the artist of the current track
-        related_artists_data, existing_related_pairs = process_related_artists(sp, item["track"]["artists"][0]["id"],
-                                                                               track['artist'], existing_related_pairs)
+        tracks.append(track)
+        artist_ids.append(item["track"]["artists"][0]["id"])
 
     df = pd.DataFrame(tracks)
-    related_artists_df = pd.DataFrame(related_artists_data)
-
     write_data(df, 'recent_tracks', "C:/SparkCourse/spotify/checkpoints/recent_tracks")
+
+    return artist_ids
+
+def process_related_artists(sp, artist_id, artist_name, existing_pairs):
+    related_artists_data = []
+    related_artists = sp.artist_related_artists(artist_id)
+
+    for related in related_artists['artists']:
+        pair = (artist_name, related['name'])
+        if pair not in existing_pairs:
+            related_artists_data.append({
+                'artist_name': artist_name,
+                'related_artist': related['name'],
+                'id_version': f"{uuid.uuid4()}"
+            })
+            existing_pairs.add(pair)
+    return related_artists_data, existing_pairs
+
+def populate_related_artists(artist_ids):
+    session = Session(bind=engine)
+
+    existing_pairs = {(tup.artist_name, tup.related_artist)
+                      for tup in session.query(RelatedArtists.artist_name, RelatedArtists.related_artist)}
+
+    processed_artists = set()  # New set to track processed artists
+    related_artists_data = []
+
+    for artist_id in artist_ids:
+        if artist_id not in processed_artists:  # Only process the artist if it hasn't been processed yet
+            artist = sp.artist(artist_id)
+            related_artists_data_temp, existing_pairs = process_related_artists(sp, artist['id'], artist['name'], existing_pairs)
+            related_artists_data.extend(related_artists_data_temp)
+            processed_artists.add(artist_id)  # Mark the artist as processed
+
+    related_artists_df = pd.DataFrame(related_artists_data)
     write_data(related_artists_df, 'related_artists', "C:/SparkCourse/spotify/checkpoints/related_artists")
 
     # Close the session
     session.close()
 
-    return tracks
+def update_artist_urls():
+    # Start a new session
+    session = Session(bind=engine)
+
+    # Query all artist names from the artist_image_urls table
+    existing_artists = {row.artist_name for row in session.query(ArtistsImageUrls.artist_name)}
+
+    # Query all distinct artists from the related_artists table
+    artists = session.query(RelatedArtists.artist_name).distinct()
+
+    # Initialize a list to hold the new data
+    artist_image_urls = []
+
+    for artist_name_row in artists:
+        artist_name = artist_name_row[0]  # Get the actual artist name from the Row object
+
+        # Only process the artist if it doesn't already exist in the artist_image_urls table
+        if artist_name not in existing_artists:
+            # Retrieve the artist's data from Spotify
+            artist_data = sp.search(q='artist:' + artist_name, type='artist')
+
+            # Get the artist's Spotify image URL and add it to artist_image_urls
+            try:
+                artist_image_url = artist_data['artists']['items'][0]['images'][0]['url']
+            except IndexError:  # Handle cases where the artist has no images
+                artist_image_url = ''
+
+            artist_image_urls.append({'artist_name': artist_name, 'image_url': artist_image_url})
+            existing_artists.add(artist_name)  # Add the artist to existing_artists
+
+    # Convert artist_image_urls to a DataFrame
+    df = pd.DataFrame(artist_image_urls)
+
+    # Update the existing table
+    df.to_sql('artist_image_urls', engine, if_exists='append')
+
+    # Close the session
+    session.close()
+
 
 # The get_top_artists() and get_top_tracks functions need a time range, which will be either short, medium or long term
 terms = ["short_term", "medium_term", "long_term"]
@@ -265,16 +227,15 @@ terms = ["short_term", "medium_term", "long_term"]
 # Start timer to check script execution time
 start_time = time.time()
 
-
-
-
+# try:
+artist_ids = []
 try:
     for term in terms:
-        get_top_tracks(term)
-        get_top_artists(term)
+        artist_ids.extend(get_top_artists(term))
 
-    get_recent_tracks()
-
+    artist_ids.extend(get_recent_tracks())
+    populate_related_artists(artist_ids)
+    update_artist_urls()
 
 except spotipy.SpotifyException as e:
     print(f"An error has occurred with Spotipy: {e}")
